b3c6a0e9913fa3846e6a79d78f95013c055e610b
Jack Gugel <jagu2118@colorado.edu>
2019-09-27 05:00:07 +0000

    Added a test to for a random calculation using the stdev function

--- test_math_lib.py
+++ test_math_lib.py
@@ -6,22 +6,27 @@ import statistics
 
 class TestMathLib(unittest.TestCase)
 
+
     def mean_None_list(self):
         r = math_lib.list_mean(None)
         self.assertEqual(r, None)
         
+
     def stdev_None_list(self):
         r = math_lib.list_stdev(None)
         self.assertEqual(r, None)
-        
+       
+
     def mean_empty_list(self):
         r = math_lib.list_mean([])
         self.assertEqual(r, None)
-        
+       
+
     def stdev_empty_list(self):
         r = math_lib.list_stdev([])
         self.assertEqual(r, None)
         
+
     def mean_test_easy_calc(self):
         r = math_lib.list_mean([5, 5, 5, 5, 5])
         self.assertEqual(r, 5)
@@ -29,7 +34,8 @@ class TestMathLib(unittest.TestCase)
     def stdev_test_easy_calc(self):
         r = math_lib.list_mean([5, 5, 5, 5])
         self.assertEqual(r, 0)
-    
+
+
     def mean_test_random_calc(self)
         L = []
         
@@ -38,6 +44,16 @@ class TestMathLib(unittest.TestCase)
         
         r = math_lib.list_mean(L)
         self.assertEqual(r, statistics.mean(L))
+
+
+    def stdev_test_random_calc(self)
+        L = []
+        
+        for i in range(10):
+            L.append(random.randint(0, 100))
+        
+        r = math_lib.list_stdev(L)
+        self.assertEqual(r, statistics.stdev(L))
 
         
     
