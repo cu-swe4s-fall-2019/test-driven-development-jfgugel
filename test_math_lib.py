b8c52d1d1e2ec2d62e5a463bbbdb7bd1f9256170
Jack Gugel <jagu2118@colorado.edu>
2019-09-27 04:37:33 +0000

    Added a test to for an easy calculation using the mean function

--- test_math_lib.py
+++ test_math_lib.py
@@ -14,12 +14,16 @@ class TestMathLib(unittest.TestCase)
         self.assertEqual(r, None)
         
     def mean_empty_list(self):
-        r = math_lib.list_stdev([])
+        r = math_lib.list_mean([])
         self.assertEqual(r, None)
         
     def stdev_empty_list(self):
         r = math_lib.list_stdev([])
         self.assertEqual(r, None)
+        
+    def mean_test_easy_calc(self):
+        r = math_lib.list_mean([5, 5, 5, 5, 5])
+        self.assertEqual(r, 5)
     
 
 if __name__ == '__main__'
