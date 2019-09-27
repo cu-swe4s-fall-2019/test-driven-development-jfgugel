7fb1dd763fb95e0ca470518478d8cf70d59065b1
Jack Gugel <jagu2118@colorado.edu>
2019-09-27 05:28:06 +0000

    added scatter plot, histogram, and combo functions to data_viz.py

--- data_viz.py
+++ data_viz.py
@@ -1,9 +1,58 @@
+import sys
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import math_lib.py
+import get_data.py
 
-def boxplot(L, out_file_name):
+
+def boxplot(L, scatter.png):
+   
+    width=3
+    height=3
+    
+    fig = plt.figure(figsize=(width, height) dpi=300)
+    
+    ax = fig.add_subplot(1, 1, 1)
+    
+    ax.plot(X, Y, '.')
+    
+    plt.savefig(scatter.png,box_inches='tight')
+    
     pass
 
-def histogram(L, out_file_name):
+
+def histogram(L, hist.png):
+    
+    width=3
+    height=3
+    
+    fig = plt.figure(figsize=(width,height), dpi=300)
+    
+    ax = fig.add_subplot(1, 1, 1)
+    
+    ax.hist(D, '.')
+    
+    plt.savefig(hist.png,bbox_inches='tight')
+    
     pass
 
-def combo(L, out_file_name):
+
+def combo(L, combo.png):
+    
+    width=6
+    height=3
+    
+    fig = plt.figure(figsize=(width, height), dpi = 300)
+    
+    ax = fig.add_subplot(1, 2, 1)
+    
+    ax.plot(X, Y, '.')
+    
+    ax = fig.add_subplot(1, 2, 2)
+    
+    ax.hist(D, '.') 
+
+    plt.savefig(combo.png,bbox_inches='tight')
+    
     pass
