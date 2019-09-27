e8c982e2b26eba2948e663cef186c17106446ee7
Jack Gugel <jagu2118@colorado.edu>
2019-09-27 05:27:11 +0000

    added stdin function to get data

--- get_data.py
+++ get_data.py
@@ -1,3 +1,32 @@
+import sys
+import matplotlib
 
-def read_stdin_col(col_num):
-    return None
+
+
+def read_stdin_col_scatter(col_num):
+    out_file = 'scatter.png'
+    X = []
+    Y = []
+    for l in sys.stdin
+    X.append(float(A[0]))
+    Y.append(float(A[1]))
+
+
+def read_stdin_col_hist(col_num):
+    out_file = 'hist.png'
+    for l in sys.stdin
+    A = l.rstrip().split()
+    D.append(float(A[0]))
+    D.append(float(A[1]))
+
+def read_stdin_col_combo(col_num):
+    out_file = 'combo.png'
+    X = []
+    Y = []
+    D = []
+    for l in sys.stdin
+    A = l.rstrip().split()
+    X.append(float(A[0]))
+    Y.append(float(A[1]))
+    D.append(float(A[0]))
+    D.append(flaot(A[1]))
